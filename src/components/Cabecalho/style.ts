7966b4fb536a99914e0b1286991cb26d8b7fe891
JVFCODE <164587661+JVFCODE@users.noreply.github.com>
2025-05-04 18:34:33 -0300

    Add files via upload

---
+++ src/components/Cabecalho/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const HeaderT = styled.h1`
+  background-color: ${(props) => props.theme.secondary};
+  color: ${(props) => props.theme.primary};
+  text-align: center;
+  padding: 24px 0;
+`
